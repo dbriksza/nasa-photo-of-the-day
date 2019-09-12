24d92e17fc829477ce3b93f8379f07f9fb6bba4f
dbriksza <dbriksza@gmail.com>
2019-09-12 13:51:30 -0400

    return accurs before axios.get still

--- src/components/Nasa.js
+++ src/components/Nasa.js
@@ -2,24 +2,31 @@ import React, { useState, useEffect } from "react";
 import NasaCard from "./NasaCard";
 import axios from "axios";
 
-export default async function Nasa() {
+export default function Nasa() {
   const [nasa, setNasa] = useState();
 
   useEffect(() => {
-    await axios
-      .get(
-        `https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&date=2012-03-14`
-      )
-      .then(response => {
+    console.log("useEffect is called");
+    const getNasa = async () => {
+      try {
+        const response = await axios.get(
+          `https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&date=2012-03-14`
+        );
         const nasainfo = response;
         console.log(nasainfo);
         setNasa(nasainfo.data);
-      })
-      .catch(error => {
+      } catch (error) {
         console.log("Sorry no space", error);
-      });
+      }
+    };
+    getNasa();
   }, []);
 
   console.log(nasa);
-  return <div className="container">{<NasaCard info={nasa} />}</div>;
+  return (
+    <div className="container">
+      {console.log(nasa)}
+      <NasaCard info={nasa} />
+    </div>
+  );
 }
